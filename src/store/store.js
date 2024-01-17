0bf297ce80c86f6e0a72cce4cc3246df0454b143
Lokesh Sai Tamiri <lokesh5g2@sasi.ac.in>
2024-01-17 13:33:58 +0530

    init

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from '../slice/authSlice';
-import loadingReducer from '../slice/loadingSlice'
-export const store = configureStore({
-    reducer: {
-        authReducer,
-        loadingReducer
-    }
-})
